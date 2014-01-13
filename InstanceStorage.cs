05d25ea6046029bed4f94e6218878b878b23c40d
Cecer <Chris@cecer1.com>
2014-01-13 18:40:20 +0000

    Allowed any type to be stored in Instance Storage

--- InstanceStorage.cs
+++ InstanceStorage.cs
@@ -4,21 +4,34 @@ using System.Collections.Generic;
 using System.Threading;
 
 using IHI.Server.Database.Actions;
+using IHI.Server.Plugins;
 
 namespace IHI.Server
 {
     public class InstanceStorage
     {
-        private readonly Dictionary<string, byte[]> _storage = new Dictionary<string, byte[]>();
-        public byte[] this[string name]
+        private readonly Dictionary<string, dynamic> _storage = new Dictionary<string,dynamic>();
+        public dynamic this[Plugin plugin, string name]
+        {
+            get
+            {
+                return this[plugin.Id + ':' + name];
+            }
+            set
+            {
+                this[plugin.Id + ':' + name] = value;
+            }
+        }
+
+        public dynamic this[string name]
         {
             get
             {
                 lock (_storage)
                 {
-                    byte[] data;
-                    if (_storage.TryGetValue(name, out data))
-                        return data;
+                    dynamic value;
+                    if (_storage.TryGetValue(name, out value))
+                        return value;
                 }
                 return null;
             }
